6002df8c45355c0ae749338dd769e8611850552b
Artemio Cabelin <artemiocabelin@gmail.com>
2017-09-16 22:14:51 -0700

    component setup

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    DashboardComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
